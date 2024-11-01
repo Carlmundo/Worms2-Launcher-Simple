82662050561cd73a9114a7801a660b19e5244ebd
Carl Norton <Carlmundo@users.noreply.github.com>
2024-11-01 21:32:48 +0000

    Initial commit

---
+++ start/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace start
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            this.Hide();           
+            var appLaunch = "frontend.exe";
+            if (File.Exists(appLaunch))
+            {
+                Process.Start(appLaunch);
+            }
+            else
+            {
+                MessageBox.Show("File not found: " + appLaunch);
+            }
+            Close();
+        }
+    }
+}
